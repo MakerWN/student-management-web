c0126ea51b5386501acafa140947187287f58719
MakerWN <weerapong18042541@gmail.com>
2024-05-13 20:18:53 +0700

    create table

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/navbar';
+
+import Dashbord from './pages/dashbord';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
